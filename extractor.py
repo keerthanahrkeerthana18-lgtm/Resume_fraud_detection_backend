03330930d3162fae421bf0b33efe5908e4ca87a6
Keerthana H R <keerthanahrkeerthana18@gmail.com>
2026-02-19 20:14:16 +0530

    trt

--- extractor.py
+++ extractor.py
@@ -4,5 +4,5 @@ def extract_text_from_pdf(file):
     text = ""
     with pdfplumber.open(file) as pdf:
         for page in pdf.pages:
-            text += page.extract_text()
+            text += page.extract_text() or ''
     return text
